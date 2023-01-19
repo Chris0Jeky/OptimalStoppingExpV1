2729643615b98e523226a426c9bc67f68959b3c0
Chris0Jeky <jeky.tck@gmail.com>
2023-01-19 15:39:12 +0000

    Percentage and List Creation overload
    Percentage and List creation functions properly set and created an overloaded version for each. Now parameters can be passed as arguments or they can be gotten as input.

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,40 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        input.hasNextInt();
-        int length = 40;
 
-        // print the list
-        System.out.println(addRandomNumbers(length));
-        // create a function that returns an integer given the percentage of a number
-        int percentage = 33;
-        int number = 40;
-        System.out.println(percentage(number, percentage));
-
-        // create a scanner to take in input from user
-
-
+        ArrayList<Integer> list = addRandomNumbers();
+        System.out.println(list);
+        System.out.println(percentageToActual(list.size()));
 
     }
 
-    private static int percentage(int number, int percentage) {
-        return (number * percentage) / 100;
+    private static int percentageToActual() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the length of the list: ");
+        int length = input.nextInt();
+        System.out.println("Enter the percentage of the list: ");
+        int percentage = input.nextInt();
+        return (length * percentage) / 100;
+    }
+
+    private static int percentageToActual(int length) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the percentage of the list: ");
+        int percentage = input.nextInt();
+        return (length * percentage) / 100;
+    }
+
+
+
+    private static ArrayList<Integer> addRandomNumbers() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the length of the list: ");
+        int length = input.nextInt();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < length; i++) {
+            list.add((int) (Math.random() * 100) + 1);
+        }
+        return list;
     }
 
     private static ArrayList<Integer> addRandomNumbers(int length) {
