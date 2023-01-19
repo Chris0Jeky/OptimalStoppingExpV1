ac9d7f9736d59cd035c7b60e6c9b8c0a1cc4c262
Chris0Jeky <jeky.tck@gmail.com>
2023-01-19 18:47:02 +0000

    Final commit. Comments
    Comments added for most of the code.

--- src/Main.java
+++ src/Main.java
@@ -4,39 +4,52 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        // creates an ArrayList of random integers and assigns it to the variable 'list'
         ArrayList<Integer> list = addRandomNumbers();
         System.out.println(list);
         System.out.println();
 
-        //create function called researchAndExploit
+        //call the function researchAndExploit and pass the list and the optimal stopping point
         researchAndExploit(list, percentageToActual(list.size()));
-
 
     }
 
+    // function that implements the research and exploit algorithm
     private static void researchAndExploit(ArrayList<Integer> list, int optimalStoppingPoint) {
+        //create a variable called highestValue and initialize it to 0
         int highestValue = 0;
+        // loop through the list from 0 to the optimal stopping point
         for (int i = 0; i < optimalStoppingPoint && i < list.size(); i++) {
+            // check if the current value is higher than the highestValue
             if (list.get(i) > highestValue) {
+                // if yes then set the highestValue as the current value
                 highestValue = list.get(i);
             }
         }
+        //loop through the list from the optimal stopping point to the end
         for (int i = optimalStoppingPoint; i < list.size(); i++) {
+            // check if the current value is greater or equal to the highestValue
             if (list.get(i) >= highestValue) {
+                // if yes then print a message
                 System.out.println("Found a better or equal value at index " + i);
                 System.out.println("The value is " + list.get(i));
                 return;
             }
         }
+        //if no better value is found
         System.out.println("No better or equal value found");
+        // set the highestValue as the last value of the list
         highestValue = list.get(list.size() - 1);
         System.out.println("The highest value is " + highestValue);
     }
 
+    // function that takes the percentage of the list and converts it to an actual number
     private static int percentageToActual() {
         Scanner input = new Scanner(System.in);
+        // ask the user for the length of the list
         System.out.println("Enter the length of the list: ");
         int length = input.nextInt();
+        // ask the user for the percentage of the list
         System.out.println("Enter the percentage of the list: ");
         int percentage = input.nextInt();
         return (length * percentage) / 100;
